085ec00b25c7576c4306a6ce2ade0825ef3e28c5
pankzid <pankzid@gmail.com>
2013-09-22 02:20:40 +0700

    Modify application layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 SampleApp3::Application.routes.draw do
   root to: 'static_pages#home'
-  get "static_pages/home", as: :home
-  get "static_pages/help", as: :help
-  get "static_pages/about", as: :about
-  match "/static_pages/contact", to: "static_pages#contact", as: :contact
+
+  match "/help",    to: 'static_pages#help'
+  match "/about",   to: 'static_pages#about'
+  match "/contact", to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
