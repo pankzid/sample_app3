ab48295a50d002730e53a93c519cc8a33b30c687
pankzid <pankzid@gmail.com>
2013-09-23 23:49:59 +0700

    Add User#new controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ SampleApp3::Application.routes.draw do
   match "/about",   to: 'static_pages#about'
   match "/contact", to: 'static_pages#contact'
 
+  match "/signup",  to: 'users#new'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
