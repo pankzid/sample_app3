98ffa493c062a46c611dfcc6628575a632f77631
pankzid <pankzid@gmail.com>
2013-09-26 10:30:33 +0700

    Do exercise chapter7: - fixing password-digest in en.yml - add size option on gravatar_for - add rspec test to after submission with valid and invalid information

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,11 @@
 module UsersHelper
+
 	# Return the gravatar (http://gravatar.com) for the given user
-	def gravatar_for(user)
+	def gravatar_for(user, option = { size: 50})
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-		gravatar_url = "https://secure.gravatar.com/avatars/#{gravatar_id}.png"
+		size = option[:size]
+		gravatar_url = "https://secure.gravatar.com/avatars/#{gravatar_id}.png?s=#{size}"
 		image_tag(gravatar_url, alt: user.name, class: 'gravatar')
 	end
+
 end
